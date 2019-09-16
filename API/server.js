c4161aee852dee62cce8bff7b178e79366dee7f2
bharti3bk <bharti3bk@gmail.com>
2019-09-16 13:12:41 -0700

    Added Endpoints and helper functions

--- API/server.js
+++ API/server.js
@@ -1,20 +1,49 @@
 const express = require("express"); 
-const server = express();
+const server = express(); 
+const Users = require('../Users/userModel.js');
+const db = require('../Data/db_config.js');
 
 server.use(express.json());    
 server.get('/' , (req,res) => {
     res.json({message:"Working ......"})
 })
 
-// server.get('/api/users' , (req, res) => {
-  
-// })  
+ server.get('/api/users' , (req, res) => {
+    Users.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(error => {
+        res.send(error);
+    })
+ })  
 
-// server.post('/api/login' , (req,res) => {
+ server.post('/api/login' , (req,res) => { 
+     const {username , password} = req.body;
+     Users.findBy({username})
+     .first()
+     .then(user => {
+         if(user) {
+             res.status(200).json({message : `${user.username}`});
+         }
+         else {
+             res.status(401).json({message : "Invalid Cred"});
+         }
+     }) 
+     .catch(error => {
+         res.status(500).json(error);
+     })
 
-// }) 
+ }) 
+ server.post('/api/register' , (req,res) => { 
+     const user = req.body;
 
-// server.post('/api/register' , (req,res) => {
-
-// })
+     Users.add(user)
+     .then(saved => {
+         res.status(201).json(saved);
+     })
+     .catch(error => {
+         res.status(500).json(error);
+     })
+})
 module.exports = server;  
--- Users/userModel.js
+++ Users/userModel.js
@@ -3,10 +3,33 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());  
-module.exports  = {
- find
-}; 
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
 
 function find() {
-    return db('users');
-  }
+  return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+function add(user) {
+  return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
+
